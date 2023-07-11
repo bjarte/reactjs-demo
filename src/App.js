e7104a8d0686f00e68e9c89de35507f0c0933af0
Bjarte Aune Olsen <bjarte@auneolsen.no>
2023-07-11 23:36:30 +0200

    Added debugger break step

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './app.css';
 
 function App() {
-
+  debugger;
   return (
     <div className="app">
       <header className="app__header">
         <img src="/logo-with-text-dark.svg" className="app__logo" alt="logo" />
-        <p>
-          We help consumers against telemarketers and scam calls.
-        </p>
+        <p>We help consumers against telemarketers and scam calls.</p>
       </header>
     </div>
   );
