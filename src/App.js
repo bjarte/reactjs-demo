efdb9f9e8afffdcf6c9693af2398809734f9168d
Bjarte Aune Olsen <bjarte@auneolsen.no>
2023-07-12 09:23:10 +0200

    Added some body content and a little styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 function App() {
   debugger;
   return (
-    <div className="app">
-      <header className="app__header">
-        <img src="/logo-with-text-dark.svg" className="app__logo" alt="logo" />
+    <div>
+      <header className="header">
+        <img src="/logo-with-text-dark.svg" className="header__logo" alt="" />
         <p>We help consumers against telemarketers and scam calls.</p>
       </header>
+      <article className="article">
+        <h1>What is a robocall?</h1>
+        <p>A robocall is a call made by a robot. A robocall is a call made by a robot. A robocall is a call made by a robot. A robocall is a call made by a robot. A robocall is a call made by a robot. A robocall is a call made by a robot. A robocall is a call made by a robot. </p>
+      </article>
     </div>
   );
-
 }
 
 export default App;
